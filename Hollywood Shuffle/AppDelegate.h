0200de5284f28496ad8d2ca2b403ddb650a7d755
Daniel Gruici <PROFESSORCORE@professorcore2.mobile.asu.edu>
2012-08-27 09:05:00 -0700

    Initial Commit

---
+++ Hollywood Shuffle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hollywood Shuffle
+//
+//  Created by Daniel Gruici on 8/27/12.
+//  Copyright (c) 2012 Daniel Gruici. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hollywood Shuffle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hollywood Shuffle
+//
+//  Created by Daniel Gruici on 8/27/12.
+//  Copyright (c) 2012 Daniel Gruici. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
